c492c2ca75e111a47f8fd0857ea8c7ceafdea296
Serhii Revva <besha2vox@gmail.com>
2023-11-28 02:28:28 +0200

    init styles theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-function App() {
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from './theme/theme';
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <header>HEADER</header>
+            <main>MAIN</main>
+            <footer>FOOTER</footer>
+        </ThemeProvider>
     );
-}
+};
 
 export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,48 @@
+export const theme = {
+    breakpoints: { mobile: '480px', tablet: '768px', desktop: '1280px' },
+    fonts: {
+        main: {
+            light: 'FiraSans-Light', // 300
+            regular: 'FiraSans-Regular', // 400
+            medium: 'FiraSans-Medium', // 500
+        },
+        secondary: {
+            regular: 'Oswald-Regular', // 400
+            medium: 'Oswald-Medium', // 500
+            bold: 'Oswald-Bold', // 700
+        },
+        logo: {
+            primary: 'PublicaPlay-Regular',
+            secondary: 'Roboto-Regular',
+        },
+    },
+    fontSizes: [
+        '10px', // 0
+        '12px', // 1
+        '14px', // 2
+        '16px', // 4
+        '18px', // 5
+        '20px', // 6
+        '24px', // 7
+        '28px', // 8
+        '32px', // 9
+        '33px', // 10
+        '36px', // 11
+        '48px', // 12
+        '64px', // 13
+        '100px', // 14
+        '164px', // 15
+    ],
+    colors: {
+        accent: '#97D28B',
+        text: '#173D33',
+        background: {
+            primary: '#F3F5FA',
+            secondary: '#173D33BF',
+            tertiary: '#173D3340',
+            burger: '#DCEFD8',
+        },
+        blure: 'backdrop-filter: blur(2px)',
+    },
+    transition: 'cubic-bezier(0.4, 0, 0.2, 1)',
+} as const;
